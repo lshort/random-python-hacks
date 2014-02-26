fcf368a6265e609800e71a79a7dd00e0f282e7b5
Lee Short <blackcat@pro-ns.net>
2014-02-25 22:14:58 -0500

    More porting

--- parking_lot.py
+++ parking_lot.py
@@ -49,250 +49,72 @@ class Distance:
 
 class Car:
     """Represents a car, within the context of a parking lot"""
-    width
-    length
-    height
-    license_plate
     def __init__(self, w, l, h, lic):
         self.width = w
         self.length = l
         self.height = h
-        self.license_plac = lic
+        self.license_plate = lic
+    width
+    length
+    height
+    license_plate
 
 
 class ParkingSpace:
     """Represents a single uncovered parking space"""
-#    width
-#    length
-#    row
-#    number
-#    occupant
     def __init__(self, w, l, r, no):
         self.width = w
         self.length = l
         self.row = r
         self.number = no
         self.occupant = None
+    width
+    length
+    row
+    number
+    occupant
 
 
-///  instances represent a single-level parking lot
-///
 class ParkingLot:
+    """Represents a single level parking lot (no height restrictions)"""
+    add_to_row( width, length, row, first, count ):
+        row_spaces = spaces_by_row[row]
+        for x in range (first, first+count):
+            row_spaces[x] = ParkingSpace(width, length, row, x)
+            available_spaces.append(row_spaces[x])
+    add_space( width, length, row, number ):
+        add_to_row(width, length, row, number, 1)
+    park_car( car ):
+        for index in range(len(available_spaces)):
+            space = available_spaces[index]
+            if is_greater(space.width, car.width) and \
+               is_greater(space.length, car.length):
+                space.occupant = car
+                license_plate_map[car.license_plate] = space
+                del available_spaces[index]
+    remove_car( license ):
+        if license in license_plate_map.keys():
+            space = license_plate_map[license]
+            space.occupant = None
+            del license_plate_map[license]
+            available_spaces.append(space)
+    find_car( license ) :
+        if license in license_plate_map.keys():
+            return license_plate_map[license]
+        else
+            return None
+    find_occupant( row, number ) :
+        return spaces_by_row[row][number]
+    printable_string():
+        retval = "Parking Lot Report\n"
+        for row in spaces_by_row.keys():
+            retval += "*Row " + row + "\n"
+            for space in spaces_by_row[row]:
+                retval += "   " + str(space) + " : " + str(space.occupant) + "\n"
+    spaces_by_row = {{}}
+    license_plate_dict = {}
+    available_spaces = []
 
-
-class parking_lot {
-public:
-  parking_lot() {};
-  parking_lot(const parking_lot &orig);   // make a deep copy
-  parking_lot(const parking_lot &&orig) ;
-  friend void swap( parking_lot& first, parking_lot& second );
-  parking_lot &operator = (parking_lot orig);
-  void add_space( const dist &w, const dist &l,
-                  const char row, const unsigned int no );
-  void add_to_row( const dist &w, const dist &l, const char row,   // add multiple
-                   const unsigned int first, const unsigned int count ); // spaces
-  bool park_car( const car &veh );
-  bool remove_car( const string &license );
-  optional<parking_space> find_car( const string &license ) const;
-  optional<car> find_occupant( char row, unsigned int no ) const;
-  void print_report(ostream &out) const;
-private:
-  // stores all the parking spaces in a single row
-  typedef map<char, vector<parking_space>> row_data;
-  row_data spaces_by_row;
-  // parking_space_ref allows us to efficiently point to entries in
-  // the vector of row_data.  we can't use a direct pointer to the
-  // individual parking_spaces, the vector could be reallocated and
-  // moved  and then the individual pointers would be invalid
-  typedef pair<vector<parking_space>*,int> parking_space_ref;
-  unordered_map<string, parking_space_ref>  license_plate_map;
-  deque<parking_space_ref> available_spaces;
-  static parking_space &get_space( const parking_space_ref &r )
-    { return  (*get<0>(r))[get<1>(r)]; };
-  parking_space_ref copy_ref( const parking_space_ref &r );
-};
-
-/**  Print out the argument to the specified ostream
-     @param[in] p The parking_space to print */
-ostream & operator << (ostream & out, const parking_lot &p ) {
-  p.print_report(out);
-  return out;
-}
-
-
-/** A function to swap two parking_lot objects
-    @param[in,out] first The first parking_lot to swap
-    @param[in,out] second The second parking lot to swap */
-void swap( parking_lot& first, parking_lot& second ) {
-  std::swap( first.spaces_by_row, second.spaces_by_row );
-  std::swap( first.license_plate_map, second.license_plate_map );
-  std::swap( first.available_spaces, second.available_spaces );
-}
-
-/** The move constructor
-    @param[in] orig The parking_lot which is assigned from */
-parking_lot::parking_lot(const parking_lot &&orig) {
-  spaces_by_row     = move(orig.spaces_by_row);
-  license_plate_map = move(orig.license_plate_map);
-  available_spaces  = move(orig.available_spaces);
-}
-
-/** The parameter 'r' points to a parking_space_ref in a different
-    parking_lot.  We build and return a parking_space_ref in our own
-    parking_lot that points to the parking_space that has the same
-    row and number as 'r' points to.  Needed for the copy constructor.
-    @param[in] r The reference that we are copying
-    @return The reference that we have built  */
-parking_lot::parking_space_ref parking_lot::copy_ref( const parking_space_ref &r ) {
-  vector<parking_space> *oldvec = get<0>(r);
-  int index = get<1>(r);
-  parking_space &space = (*oldvec)[index];
-  vector<parking_space> *newvec = &spaces_by_row[space.row];
-  return make_pair( newvec, index );
-}
-
-/** The copy constructor, makes a deep copy
-    @param[in] orig The parking_lot which is assigned from */
-parking_lot::parking_lot(const parking_lot &orig) {
-  // get our own copy of the map and vectors of data it contains
-  spaces_by_row = orig.spaces_by_row;
-  for ( auto &entry : orig.license_plate_map )  {
-    parking_space_ref spaceref = get<1>(entry);
-    license_plate_map[get<0>(entry)] = copy_ref( spaceref );
-  }
-  for ( auto &spaceref : orig.available_spaces ) {
-    available_spaces.push_back(copy_ref(spaceref));
-  }
-}
-
-/** The assignment operator
-    @param[in] orig The parking_lot which is assigned from
-    @return Returns the value to be assigned */
-parking_lot &parking_lot::operator = (parking_lot orig) {
-  swap(*this, orig);
-  return *this;
-}
-
-/** Adds a single space to the row
-    @param[in] w The width of the parking space
-    @param[in] l The length of the parking space
-    @param[in] row The row's identifying letter
-    @param[in] no The number of the parking space */
-void parking_lot::add_space(const dist &w, const dist &l, char row, unsigned int no )
-{
-  add_to_row( w, l, row, no, 1 );
-}
-
-/** Adds several parking spaces to the row, numbering them sequentially
-    @param[in] w The width of the parking spaces
-    @param[in] l The length of the parking spaces
-    @param[in] row The row's identifying letter
-    @param[in] first The number of the first parking space
-    @param[in] count The number of parking spaces to add   */
-void parking_lot::add_to_row(const dist &w, const dist &l, char row,
-                             unsigned int first, unsigned int count )
-{
-  auto mypair = spaces_by_row.find(row);
-  if ( spaces_by_row.end() == mypair )  {
-    spaces_by_row[row] = vector<parking_space>();
-    mypair = spaces_by_row.find(row);
-  }
-  vector<parking_space> &vec = get<1>(*mypair);
-  for (unsigned int i=first; i<first+count; ++i)  {
-    vec.push_back( parking_space{ w, l, row, i, no_car } );
-    available_spaces.push_back( make_pair(&vec,vec.size()-1) );
-  }
-}
-
-/** Parks the car in an appropriately sized parking space
-    @param[in] veh The car
-    @return Returns false if no space was available, true otherwise */
-bool parking_lot::park_car( const car &veh )
-{
-  dist  l = veh.length;
-  dist  w = veh.width;
-  auto spaceref = find_if( available_spaces.begin(), available_spaces.end(),
-             [&l,&w, this] (parking_space_ref ref)  {
-             parking_space &p = get_space(ref);
-             return (   is_greater(p.length,l)
-                     && is_greater(p.width,w)  ); });
-  if ( available_spaces.end() != spaceref ) {
-    auto &space = get_space( *spaceref );
-    space.occupant = optional<car>(veh);
-    license_plate_map[veh.license_plate] = *spaceref;
-    available_spaces.erase(spaceref);
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/** Removes the car with the specified license plate
-    @param[in] license The license plate to search for
-    @return Returns true if it found a matching car, false otherwise */
-bool parking_lot::remove_car( const string &license )
-{
-  auto entry = license_plate_map.find(license);
-  if ( entry == license_plate_map.end() )  {
-    return false;
-  }
-  parking_space &space = get_space(get<1>(*entry));
-  space.occupant = no_car;
-  license_plate_map.erase(license);
-  available_spaces.push_back(get<1>(*entry));
-  return true;
-}
-
-/** Prints a report
-    @param[in] out The stream to print the report to */
-void parking_lot::print_report(ostream &out) const
-{
-  out << endl << endl << "Parking Lot Report" << endl;
-  for ( auto &mypair : spaces_by_row ) {
-    out << "  Row " << get<0>(mypair) << endl;
-    for ( auto &space : get<1>(mypair) )  {
-      out << "    Space " << space.number << " : ";
-      if ( !space.occupant )  {
-        out << "empty" << endl;
-      }  else  {
-        out << space.occupant.get().license_plate << endl;
-      }
-    }
-  }
-  out << endl;
-}
-
-/**  Find the car the occupies a specific parking space
-     @param[in] row  The row of the parking space
-     @param[in] no   The number of the parking space
-     @return If not found -- the empty object
-     @return If found -- the car data */
-optional<car> parking_lot::find_occupant( char row, unsigned int no ) const
-{
-  auto mypair = spaces_by_row.find(row);
-  if ( spaces_by_row.end() != mypair )  {
-    const vector<parking_space> &vec = get<1>(*mypair);
-    for (auto &space : vec)
-      if ( space.number == no )
-        return space.occupant;
-  }
-  return no_car;
-}
-
-/**  Find a car with the specified license plate
-     @param [in] license The license plate to find
-     @return If not found -- the empty object
-     @return If found -- the parking space data */
-optional<parking_space> parking_lot::find_car( const string &license ) const {
-  try {
-    const parking_space_ref &ref = license_plate_map.at(license);
-    vector<parking_space> *vec = get<0>(ref);
-    int index = get<1>(ref);
-    return optional<parking_space>((*vec)[index]);
-  } catch (const std::out_of_range & out) {
-    return optional<parking_space>();
-  }
-}
 
 
 
