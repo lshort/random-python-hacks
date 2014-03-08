96812e3041f6c386c7035514631e3636b6847bff
lshort <blackcat@pro-ns.net>
2014-03-07 17:00:06 -0800

    Follows the NEXT 100 links

--- scrapy/craigslist_jobs/craigslist_jobs/spiders/spider.py
+++ scrapy/craigslist_jobs/craigslist_jobs/spiders/spider.py
@@ -1,19 +1,29 @@
-from scrapy.spider import Spider
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from craigslist_jobs.items import CraigslistJobsItem
 
-class JobSpider(Spider):
-    name = "craiglist_jobs"
+class JobSpider(CrawlSpider):
+    name = "craigslist_jobs"
     allowed_domains = ["craigslist.org"]
     start_urls = ["http://seattle.craigslist.org/sof/"]
 
-    def parse(self, response):
+    extractor = SgmlLinkExtractor(allow=("index\d00\.html"), restrict_xpaths=('//p[@class="nextpage"]',) )
+
+    rules = (
+        Rule(extractor,callback='parse_links',follow=True),
+       )
+
+    def parse_start_url(self, response):
+        return list(self.parse_links(response))
+
+    def parse_links(self, response):
         hxs = Selector(response)
-        titles = hxs.xpath("//span[@class='pl']")
         items = []
+        titles = hxs.xpath('//span[@class="pl"]')
         for title in titles:
-            item = CraigslistJobItem()
-            item['title'] = titles.xpath("a/text()").extract()
-            item['link'] = titles.xpath("a/@href").extract()
+            item = CraigslistJobsItem()
+            item['title'] = title.xpath("a/text()").extract()
+            item['link'] = title.xpath("a/@href").extract()
             items.append(item)
         return items
