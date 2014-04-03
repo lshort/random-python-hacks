4f9bfe9036e4562ad6c565f54548e6c4185bedec
lshort <blackcat@pro-ns.net>
2014-04-03 09:39:45 -0700

    adding docstrings

--- random_text.py
+++ random_text.py
@@ -1,18 +1,21 @@
-""""""
+"""Some fun with random text generation"""
 
 import random
 from nltk import *
 from nltk.util import trigrams
-from nltk.corpus import brown
 from nltk.book import text1, text2, text3
 
 def random_text_bigram(corpus, sentence_count, delim):
-    print "*** Starting random text"
+    """Generate some random sentences in the style of the corpus.
+    Uses a bigram distribution"""
+    print "*** Starting random text bigram"
     big = bigrams(corpus)
     (word,ignore) = random.choice(big)
     cfd = ConditionalFreqDist(
         (first,second)
         for (first, second) in big[:-1])
+        # must remove the last bigram, as we may not be able to
+        # proceed from the very last word in the corpus
     para = ""
     while (sentence_count > 0):
         para += word
@@ -25,12 +28,16 @@ def random_text_bigram(corpus, sentence_count, delim):
     print para
 
 def random_text_trigram(corpus, sentence_count, delim):
+    """Generate some random sentences in the style of the corpus.
+    Uses a trigram distribution"""
     print "*** Starting random text trigram"
     tri = trigrams(corpus)
     (prev_word,crnt_word,ignore) = random.choice(tri)
     cfd = ConditionalFreqDist(
         ((first,second),third)
-        for (first, second, third) in tri[:-1])
+        for (first, second, third) in tri[:-2])
+        # must remove the last 2 trigrams, as we may not be able to
+        # proceed from the last 2 words in the corpus
     para = ""
     while (sentence_count > 0):
         para += crnt_word
