290b44b05b4be31352f49915ae127d998d30869f
lshort <blackcat@pro-ns.net>
2014-04-03 09:28:40 -0700

    adding generation with trigrams

--- random_text.py
+++ random_text.py
@@ -2,16 +2,17 @@
 
 import random
 from nltk import *
+from nltk.util import trigrams
 from nltk.corpus import brown
 from nltk.book import text1, text2, text3
 
-def random_text(corpus, sentence_count, delim):
+def random_text_bigram(corpus, sentence_count, delim):
     print "*** Starting random text"
     big = bigrams(corpus)
     (word,ignore) = random.choice(big)
     cfd = ConditionalFreqDist(
         (first,second)
-        for (first, second) in big)
+        for (first, second) in big[:-1])
     para = ""
     while (sentence_count > 0):
         para += word
@@ -23,6 +24,28 @@ def random_text(corpus, sentence_count, delim):
         word = random.choice(cfd[word].samples())
     print para
 
+def random_text_trigram(corpus, sentence_count, delim):
+    print "*** Starting random text trigram"
+    tri = trigrams(corpus)
+    (prev_word,crnt_word,ignore) = random.choice(tri)
+    cfd = ConditionalFreqDist(
+        ((first,second),third)
+        for (first, second, third) in tri[:-1])
+    para = ""
+    while (sentence_count > 0):
+        para += crnt_word
+        if crnt_word[0] in ".!?":
+            sentence_count -= 1
+            para += delim
+        else:
+            para += " "
+        index = (prev_word,crnt_word)
+        prev_word = crnt_word
+        crnt_word = random.choice(cfd[index].samples())
+    print para
+
 if __name__=="__main__":
-    random_text(text1,4, "___")
-    random_text(text2,3, "  ")
+    random_text_bigram(text1,4, "  ")
+    random_text_bigram(text2,3, "  ")
+    random_text_trigram(text1,4, "  ")
+    random_text_trigram(text2,3, "  ")
