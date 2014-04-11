a240c3ac9bb9b9e74293338f9da40d6cf6dac8bb
lshort <blackcat@pro-ns.net>
2014-04-11 14:10:40 -0700

    better filtering

--- unique_words.py
+++ unique_words.py
@@ -6,10 +6,13 @@ import re
 from nltk.corpus import words
 
 def unique_words(url):
-    raw = urllib.urlopen(url).read()
-    url_words = [w for w in nltk.word_tokenize(raw) if re.search('\w+',w)]
+    raw = nltk.clean_html(urllib.urlopen(url).read())
+    raw = raw[:8192]    # take a sample for testing purposes
+    pattern = r'(\b[A-Z]+\b)|(\b[A-Za-z][a-z]*(-[a-z]+)*\b)'
+    url_words = nltk.regexp_tokenize(raw, pattern)
+    print url_words
     dict_words = words.words('en')
     return [w for w in url_words if w not in dict_words]
 
 if __name__=="__main__":
-    print unique_words('http://news.google.com')
+    print unique_words('http://news.bbc.co.uk')
