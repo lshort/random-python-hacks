265b3a67864dfe4b368f8570675b69a456fc16c0
lshort <blackcat@pro-ns.net>
2014-04-25 14:56:15 -0700

    using filter

--- unique_words.py
+++ unique_words.py
@@ -12,16 +12,21 @@ def find_words(string):
     ['REM', 'monitor', 'Mount', 'Everest', 'plain-text']
     """
     pattern = r'(\b[A-Z]+\b)|(\b[A-Za-z][a-z]*(-[a-z]+)*\b)'
-    url_words = nltk.regexp_tokenize(raw, pattern)
+    url_words = nltk.regexp_tokenize(string, pattern)
+    return url_words
 
+def search(substring, words):
+    for word in words:
+        if substring in word:
+            yield word
 
 def unique_words(url):
     """Find words in the url text that are not in the 'words' corpus"""
     raw = nltk.clean_html(urllib.urlopen(url).read())
     url_words = find_words(raw[:8192])    # take a sample for testing purposes
-    print url_words
     dict_words = words.words('en')
-    return [w for w in url_words if w not in dict_words and w.lower() not in dict_words]
+    return filter(lambda w:w not in dict_words and w.lower() not in dict_words,
+                  url_words )
 
 if __name__=="__main__":
     ws = unique_words('http://news.bbc.co.uk')
